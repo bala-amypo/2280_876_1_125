3cbbb770dc1654bb49af797208ddea8905f18ee4
bala-amypo <bala@amypo.in>
2025-12-23 04:48:26 +0000

    User commit

--- src/main/java/com/example/demo/entity/MenuItem.java
+++ src/main/java/com/example/demo/entity/MenuItem.java
@@ -3,22 +3,31 @@ package com.example.demo.entity;
 import jakarta.persistence.*;
 import java.math.BigDecimal;
 import java.sql.Timestamp;
+import java.util.Set;
 
 @Entity
 @Table(uniqueConstraints = @UniqueConstraint(columnNames = "name"))
-public class Ingredient {
+public class MenuItem {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String name;
-    private String unit;
-    private BigDecimal costPerUnit;
+    private String description;
+    private BigDecimal sellingPrice;
     private Boolean active = true;
 
     private Timestamp createdAt;
     private Timestamp updatedAt;
+
+    @ManyToMany
+    @JoinTable(
+        name = "menu_item_category",
+        joinColumns = @JoinColumn(name = "menu_item_id"),
+        inverseJoinColumns = @JoinColumn(name = "category_id")
+    )
+    private Set<Category> categories;
 
     @PrePersist
     void onCreate() {
