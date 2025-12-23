1c5d5e839698e36bf95e7e12e3499232595f15fe
bala-amypo <bala@amypo.in>
2025-12-23 06:18:26 +0000

    User commit

--- src/main/java/com/example/demo/controller/IngredientController.java
+++ src/main/java/com/example/demo/controller/IngredientController.java
@@ -1,45 +1,45 @@
 package com.example.demo.controller;
 
-import com.example.demo.entity.Ingredient;
-import com.example.demo.service.IngredientService;
-import io.swagger.v3.oas.annotations.tags.Tag;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
+import com.example.demo.entity.Ingredient;
+import com.example.demo.service.IngredientService;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequestMapping("/api/ingredients")
+@RequiredArgsConstructor
 @Tag(name = "Ingredients")
 public class IngredientController {
 
-    private final IngredientService service;
-
-    public IngredientController(IngredientService service) {
-        this.service = service;
-    }
+    private final IngredientService ingredientService;
 
     @PostMapping
-    public Ingredient create(@RequestBody Ingredient i) {
-        return service.createIngredient(i);
+    public Ingredient create(@RequestBody Ingredient ingredient) {
+        return ingredientService.createIngredient(ingredient);
     }
 
     @PutMapping("/{id}")
-    public Ingredient update(@PathVariable Long id, @RequestBody Ingredient i) {
-        return service.updateIngredient(id, i);
+    public Ingredient update(@PathVariable Long id, @RequestBody Ingredient ingredient) {
+        return ingredientService.updateIngredient(id, ingredient);
     }
 
     @GetMapping("/{id}")
     public Ingredient get(@PathVariable Long id) {
-        return service.getIngredientById(id);
+        return ingredientService.getIngredientById(id);
     }
 
     @GetMapping
-    public List<Ingredient> all() {
-        return service.getAllIngredients();
+    public List<Ingredient> getAll() {
+        return ingredientService.getAllIngredients();
     }
 
     @PutMapping("/{id}/deactivate")
     public void deactivate(@PathVariable Long id) {
-        service.deactivateIngredient(id);
+        ingredientService.deactivateIngredient(id);
     }
 }
