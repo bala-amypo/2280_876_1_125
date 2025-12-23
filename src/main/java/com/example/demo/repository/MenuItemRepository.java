dcc3239ccaee4e5ed5fe1bacbccda15adacdb39b
bala-amypo <bala@amypo.in>
2025-12-23 09:00:19 +0000

    User commit

--- src/main/java/com/example/demo/repository/MenuItemRepository.java
+++ src/main/java/com/example/demo/repository/MenuItemRepository.java
@@ -3,8 +3,5 @@ package com.example.demo.repository;
 import com.example.demo.entity.MenuItem;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
 public interface MenuItemRepository extends JpaRepository<MenuItem, Long> {
-    Optional<MenuItem> findByNameIgnoreCase(String name);
 }
--- src/main/java/com/example/demo/repository/RecipeIngredientRepository.java
+++ src/main/java/com/example/demo/repository/RecipeIngredientRepository.java
@@ -3,9 +3,5 @@ package com.example.demo.repository;
 import com.example.demo.entity.RecipeIngredient;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
 public interface RecipeIngredientRepository extends JpaRepository<RecipeIngredient, Long> {
-    List<RecipeIngredient> findByMenuItemId(Long menuItemId);
-    boolean existsByMenuItemId(Long menuItemId);
 }
