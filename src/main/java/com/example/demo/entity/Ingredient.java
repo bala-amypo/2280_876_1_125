7470cfe610ea28e397c7e2d9eb4cc76ed527c873
bala-amypo <bala@amypo.in>
2025-12-23 05:52:29 +0000

    User commit

--- src/main/java/com/example/demo/entity/Ingredient.java
+++ src/main/java/com/example/demo/entity/Ingredient.java
@@ -3,8 +3,13 @@ package com.example.demo.entity;
 import jakarta.persistence.*;
 import java.math.BigDecimal;
 import java.sql.Timestamp;
+import lombok.*;
 
 @Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 @Table(uniqueConstraints = @UniqueConstraint(columnNames = "name"))
 public class Ingredient {
 
