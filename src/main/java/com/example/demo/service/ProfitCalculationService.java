e9cc698d64fe1a345f5bac5598fb0dc5334b2022
bala-amypo <bala@amypo.in>
2025-12-24 05:05:10 +0000

    User commit

--- src/main/java/com/example/demo/service/ProfitCalculationService.java
+++ src/main/java/com/example/demo/service/ProfitCalculationService.java
@@ -1,15 +1,16 @@
 package com.example.demo.service;
 
 import com.example.demo.entity.ProfitCalculationRecord;
+
 import java.util.List;
 
 public interface ProfitCalculationService {
-
     ProfitCalculationRecord calculateProfit(Long menuItemId);
-
     ProfitCalculationRecord getCalculationById(Long id);
-
-    List<ProfitCalculationRecord> getByMenuItem(Long menuItemId);
-
+    List<ProfitCalculationRecord> getCalculationsForMenuItem(Long menuItemId);
     List<ProfitCalculationRecord> getAllCalculations();
+    List<ProfitCalculationRecord> findRecordsWithMarginBetween(Double min, Double max);
+
+    // Add this line to fix @Override error
+    List<ProfitCalculationRecord> getByMenuItem(Long menuItemId);
 }
