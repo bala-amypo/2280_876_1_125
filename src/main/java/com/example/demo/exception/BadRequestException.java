37d02a43731ac5dbc013c16eca3d127e062ee62b
bala-amypo <bala@amypo.in>
2025-12-24 04:05:10 +0000

    User commit

--- src/main/java/com/example/demo/exception/BadRequestException.java
+++ src/main/java/com/example/demo/exception/BadRequestException.java
@@ -1,7 +1,7 @@
-// package com.example.demo.exception;
+package com.example.demo.exception;
 
-// public class BadRequestException extends RuntimeException {
-//     public BadRequestException(String msg) {
-//         super(msg);
-//     }
-// }
+public class BadRequestException extends RuntimeException {
+    public BadRequestException(String msg) {
+        super(msg);
+    }
+}
--- src/main/java/com/example/demo/security/JwtTokenProvider.java
+++ src/main/java/com/example/demo/security/JwtTokenProvider.java
@@ -1,27 +1,27 @@
-// package com.example.demo.security;
+package com.example.demo.security;
 
-// import io.jsonwebtoken.*;
-// import java.util.Date;
+import io.jsonwebtoken.*;
+import java.util.Date;
 
-// public class JwtTokenProvider {
+public class JwtTokenProvider {
 
-//     private final String secret;
-//     private final long validityInMs;
+    private final String secret;
+    private final long validityInMs;
 
-//     public JwtTokenProvider(String secret, long validityInMs) {
-//         this.secret = secret;
-//         this.validityInMs = validityInMs;
-//     }
+    public JwtTokenProvider(String secret, long validityInMs) {
+        this.secret = secret;
+        this.validityInMs = validityInMs;
+    }
 
-//     public String createToken(String email) {
-//         Date now = new Date();
-//         Date exp = new Date(now.getTime() + validityInMs);
+    public String createToken(String email) {
+        Date now = new Date();
+        Date exp = new Date(now.getTime() + validityInMs);
 
-//         return Jwts.builder()
-//                 .setSubject(email)
-//                 .setIssuedAt(now)
-//                 .setExpiration(exp)
-//                 .signWith(SignatureAlgorithm.HS256, secret)
-//                 .compact();
-//     }
-// }
+        return Jwts.builder()
+                .setSubject(email)
+                .setIssuedAt(now)
+                .setExpiration(exp)
+                .signWith(SignatureAlgorithm.HS256, secret)
+                .compact();
+    }
+}
