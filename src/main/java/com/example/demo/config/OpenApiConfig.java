92e54a4b9b6773a588d1e033854286eeb8915e9d
bala-amypo <bala@amypo.in>
2025-12-30 04:38:16 +0000

    User commit

--- src/main/java/com/example/demo/config/OpenApiConfig.java
+++ src/main/java/com/example/demo/config/OpenApiConfig.java
@@ -25,7 +25,7 @@ public class OpenApiConfig {
 
                 // Server configuration
                 .servers(List.of(
-                        new Server().url("https://9169.32procr.amypo.ai/swagger-ui/index.html")
+                        new Server().url("https://9169.32procr.amypo.ai/")
                 ))
 
                 // Security requirement
